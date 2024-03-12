b3b1a9da8a0d868d1a0da2c822c3f87b361d173c
yyonyy <123567814+yyonyy@users.noreply.github.com>
2024-03-12 19:01:53 +0900

    commmit

---
+++ PathPlanning/a_star_for_study.py
@@ -0,0 +1,634 @@
+import numpy as np
+import math
+import bisect
+import matplotlib.pyplot as plt
+
+from scipy.optimize import curve_fit
+
+show_animation = True
+
+
+class AStarPlanner:
+# AStarPlanner(ox, oy, grid_size, robot_radius)
+    def __init__(self, ox, oy, resolution, rr):
+        """
+        Initialize grid map for a star planning
+
+        ox: x position list of Obstacles [m]
+        oy: y position list of Obstacles [m]
+        resolution: grid resolution [m]
+        rr: robot radius[m]
+        """
+
+        self.resolution = resolution
+        self.rr = rr
+        self.min_x, self.min_y = 0, 0
+        self.max_x, self.max_y = 0, 0
+        self.obstacle_map = None
+        self.x_width, self.y_width = 0, 0
+        self.motion = self.get_motion_model()
+        self.calc_obstacle_map(ox, oy)
+
+    class Node:
+        def __init__(self, x, y, cost, parent_index):
+            self.x = x  # index of grid
+            self.y = y  # index of grid
+            self.cost = cost
+            self.parent_index = parent_index
+
+        def __str__(self):
+            return str(self.x) + "," + str(self.y) + "," + str(
+                self.cost) + "," + str(self.parent_index)
+
+    def planning(self, sx, sy, gx, gy):
+        """
+        A star path search
+
+        input:
+            s_x: start x position [m]
+            s_y: start y position [m]
+            gx: goal x position [m]
+            gy: goal y position [m]
+
+        output:
+            rx: x position list of the final path
+            ry: y position list of the final path
+        """
+
+        start_node = self.Node(self.calc_xy_index(sx, self.min_x),self.calc_xy_index(sy, self.min_y), 0.0, -1)
+        goal_node = self.Node(self.calc_xy_index(gx, self.min_x),self.calc_xy_index(gy, self.min_y), 0.0, -1)
+
+        open_set, closed_set = dict(), dict()
+        open_set[self.calc_grid_index(start_node)] = start_node
+        
+        while True:
+            if len(open_set) == 0:
+                print("Open set is empty..")
+                break
+
+            c_id = min(open_set,key=lambda o: open_set[o].cost + self.calc_heuristic(goal_node,open_set[o]))
+            
+            current = open_set[c_id]
+            print('current::',current)
+
+            # show graph
+            if show_animation:  # pragma: no cover
+                plt.plot(self.calc_grid_position(current.x, self.min_x),
+                         self.calc_grid_position(current.y, self.min_y), "xc")
+                # for stopping simulation with the esc key.
+                plt.gcf().canvas.mpl_connect('key_release_event',
+                                             lambda event: [exit(
+                                                 0) if event.key == 'escape' else None])
+                if len(closed_set.keys()) % 10 == 0:
+                    plt.pause(0.001)
+
+            if current.x == goal_node.x and current.y == goal_node.y:
+                print("Find goal")
+                goal_node.parent_index = current.parent_index
+                goal_node.cost = current.cost
+                break
+
+            # Remove the item from the open set
+            del open_set[c_id]
+
+            # Add it to the closed set
+            closed_set[c_id] = current
+
+            # expand_grid search grid based on motion model
+            for i, _ in enumerate(self.motion):
+                node = self.Node(current.x + self.motion[i][0],
+                                 current.y + self.motion[i][1],
+                                 current.cost + self.motion[i][2], c_id)
+                n_id = self.calc_grid_index(node)
+
+                # If the node is not safe, do nothing
+                if not self.verify_node(node):
+                    continue
+
+                if n_id in closed_set:
+                    continue
+
+                if n_id not in open_set:
+                    open_set[n_id] = node  # discovered a new node
+                else:
+                    if open_set[n_id].cost > node.cost:
+                        # This path is the best until now. record it
+                        open_set[n_id] = node
+
+        rx, ry = self.calc_final_path(goal_node, closed_set)
+
+        return rx, ry
+
+    def calc_final_path(self, goal_node, closed_set):
+        # generate final course
+        rx, ry = [self.calc_grid_position(goal_node.x, self.min_x)], [
+            self.calc_grid_position(goal_node.y, self.min_y)]
+        parent_index = goal_node.parent_index
+        while parent_index != -1:
+            n = closed_set[parent_index]
+            rx.append(self.calc_grid_position(n.x, self.min_x))
+            ry.append(self.calc_grid_position(n.y, self.min_y))
+            parent_index = n.parent_index
+
+        return rx, ry
+
+    @staticmethod
+    def calc_heuristic(n1, n2):
+        w = 1.0  # weight of heuristic
+        d = w * math.hypot(n1.x - n2.x, n1.y - n2.y)
+        return d
+
+    def calc_grid_position(self, index, min_position):
+        """
+        calc grid position
+
+        :param index:
+        :param min_position:
+        :return:
+        """
+        pos = index * self.resolution + min_position
+        return pos
+
+    def calc_xy_index(self, position, min_pos):
+        return round((position - min_pos) / self.resolution)
+
+    def calc_grid_index(self, node):
+        return (node.y - self.min_y) * self.x_width + (node.x - self.min_x)
+
+    def verify_node(self, node):
+        px = self.calc_grid_position(node.x, self.min_x)
+        py = self.calc_grid_position(node.y, self.min_y)
+
+        if px < self.min_x:
+            return False
+        elif py < self.min_y:
+            return False
+        elif px >= self.max_x:
+            return False
+        elif py >= self.max_y:
+            return False
+
+        # collision check
+        if self.obstacle_map[node.x][node.y]:
+            return False
+
+        return True
+
+    def calc_obstacle_map(self, ox, oy):
+
+        self.min_x = round(min(ox))
+        self.min_y = round(min(oy))
+        self.max_x = round(max(ox))
+        self.max_y = round(max(oy))
+        print("min_x:", self.min_x)
+        print("min_y:", self.min_y)
+        print("max_x:", self.max_x)
+        print("max_y:", self.max_y)
+
+        self.x_width = round((self.max_x - self.min_x) / self.resolution)
+        self.y_width = round((self.max_y - self.min_y) / self.resolution)
+        print("x_width:", self.x_width)
+        print("y_width:", self.y_width)
+
+        # obstacle map generation
+        self.obstacle_map = [[False for _ in range(self.y_width)]
+                             for _ in range(self.x_width)]
+        for ix in range(self.x_width):
+            x = self.calc_grid_position(ix, self.min_x)
+            for iy in range(self.y_width):
+                y = self.calc_grid_position(iy, self.min_y)
+                for iox, ioy in zip(ox, oy):
+                    d = math.hypot(iox - x, ioy - y)
+                    if d <= self.rr:
+                        self.obstacle_map[ix][iy] = True
+                        break
+
+    @staticmethod
+    def get_motion_model():
+        # dx, dy, cost
+        motion = [[1, 0, 1],
+                  [0, 1, 1],
+                  [-1, 0, 1],
+                  [0, -1, 1],
+                  [-1, -1, math.sqrt(2)],
+                  [-1, 1, math.sqrt(2)],
+                  [1, -1, math.sqrt(2)],
+                  [1, 1, math.sqrt(2)]]
+
+        return motion
+
+def func(x, a, b, c, d, e, f, g, h):
+    return a * x ** 8 + b * x ** 7 + c * x ** 6 + d * x ** 5 + e * x ** 4 + f * x ** 3 + g * x ** 2 + h * x
+
+class CubicSpline1D:
+    """
+    1D Cubic Spline class
+
+    Parameters
+    ----------
+    x : list
+        x coordinates for data points. This x coordinates must be
+        sorted
+        in ascending order.
+    y : list
+        y coordinates for data points
+
+    Examples
+    --------
+    You can interpolate 1D data points.
+
+    >>> import numpy as np
+    >>> import matplotlib.pyplot as plt
+    >>> x = np.arange(5)
+    >>> y = [1.7, -6, 5, 6.5, 0.0]
+    >>> sp = CubicSpline1D(x, y)
+    >>> xi = np.linspace(0.0, 5.0)
+    >>> yi = [sp.calc_position(x) for x in xi]
+    >>> plt.plot(x, y, "xb", label="Data points")
+    >>> plt.plot(xi, yi , "r", label="Cubic spline interpolation")
+    >>> plt.grid(True)
+    >>> plt.legend()
+    >>> plt.show()
+
+    .. image:: cubic_spline_1d.png
+
+    """
+
+    def __init__(self, x, y):
+
+        h = np.diff(x)
+        if np.any(h < 0):
+            raise ValueError("x coordinates must be sorted in ascending order")
+
+        self.a, self.b, self.c, self.d = [], [], [], []
+        self.x = x
+        self.y = y
+        self.nx = len(x)  # dimension of x
+
+        # calc coefficient a
+        self.a = [iy for iy in y]
+
+        # calc coefficient c
+        A = self.__calc_A(h)
+        B = self.__calc_B(h, self.a)
+        self.c = np.linalg.solve(A, B)
+
+        # calc spline coefficient b and d
+        for i in range(self.nx - 1):
+            d = (self.c[i + 1] - self.c[i]) / (3.0 * h[i])
+            b = 1.0 / h[i] * (self.a[i + 1] - self.a[i]) \
+                - h[i] / 3.0 * (2.0 * self.c[i] + self.c[i + 1])
+            self.d.append(d)
+            self.b.append(b)
+
+    def calc_position(self, x):
+        """
+        Calc `y` position for given `x`.
+
+        if `x` is outside the data point's `x` range, return None.
+
+        Returns
+        -------
+        y : float
+            y position for given x.
+        """
+        if x < self.x[0]:
+            return None
+        elif x > self.x[-1]:
+            return None
+
+        i = self.__search_index(x)
+        dx = x - self.x[i]
+        position = self.a[i] + self.b[i] * dx + \
+            self.c[i] * dx ** 2.0 + self.d[i] * dx ** 3.0
+
+        return position
+
+    def calc_first_derivative(self, x):
+        """
+        Calc first derivative at given x.
+
+        if x is outside the input x, return None
+
+        Returns
+        -------
+        dy : float
+            first derivative for given x.
+        """
+
+        if x < self.x[0]:
+            return None
+        elif x > self.x[-1]:
+            return None
+
+        i = self.__search_index(x)
+        dx = x - self.x[i]
+        dy = self.b[i] + 2.0 * self.c[i] * dx + 3.0 * self.d[i] * dx ** 2.0
+        return dy
+
+    def calc_second_derivative(self, x):
+        """
+        Calc second derivative at given x.
+
+        if x is outside the input x, return None
+
+        Returns
+        -------
+        ddy : float
+            second derivative for given x.
+        """
+
+        if x < self.x[0]:
+            return None
+        elif x > self.x[-1]:
+            return None
+
+        i = self.__search_index(x)
+        dx = x - self.x[i]
+        ddy = 2.0 * self.c[i] + 6.0 * self.d[i] * dx
+        return ddy
+
+    def __search_index(self, x):
+        """
+        search data segment index
+        """
+        return bisect.bisect(self.x, x) - 1
+
+    def __calc_A(self, h):
+        """
+        calc matrix A for spline coefficient c
+        """
+        A = np.zeros((self.nx, self.nx))
+        A[0, 0] = 1.0
+        for i in range(self.nx - 1):
+            if i != (self.nx - 2):
+                A[i + 1, i + 1] = 2.0 * (h[i] + h[i + 1])
+            A[i + 1, i] = h[i]
+            A[i, i + 1] = h[i]
+
+        A[0, 1] = 0.0
+        A[self.nx - 1, self.nx - 2] = 0.0
+        A[self.nx - 1, self.nx - 1] = 1.0
+        return A
+
+    def __calc_B(self, h, a):
+        """
+        calc matrix B for spline coefficient c
+        """
+        B = np.zeros(self.nx)
+        for i in range(self.nx - 2):
+            B[i + 1] = 3.0 * (a[i + 2] - a[i + 1]) / h[i + 1]\
+                - 3.0 * (a[i + 1] - a[i]) / h[i]
+        return B
+
+class CubicSpline2D:
+    """
+    Cubic CubicSpline2D class
+
+    Parameters
+    ----------
+    x : list
+        x coordinates for data points.
+    y : list
+        y coordinates for data points.
+
+    Examples
+    --------
+    You can interpolate a 2D data points.
+
+    >>> import matplotlib.pyplot as plt
+    >>> x = [-2.5, 0.0, 2.5, 5.0, 7.5, 3.0, -1.0]
+    >>> y = [0.7, -6, 5, 6.5, 0.0, 5.0, -2.0]
+    >>> ds = 0.1  # [m] distance of each interpolated points
+    >>> sp = CubicSpline2D(x, y)
+    >>> s = np.arange(0, sp.s[-1], ds)
+    >>> rx, ry, ryaw, rk = [], [], [], []
+    >>> for i_s in s:
+    ...     ix, iy = sp.calc_position(i_s)
+    ...     rx.append(ix)
+    ...     ry.append(iy)
+    ...     ryaw.append(sp.calc_yaw(i_s))
+    ...     rk.append(sp.calc_curvature(i_s))
+    >>> plt.subplots(1)
+    >>> plt.plot(x, y, "xb", label="Data points")
+    >>> plt.plot(rx, ry, "-r", label="Cubic spline path")
+    >>> plt.grid(True)
+    >>> plt.axis("equal")
+    >>> plt.xlabel("x[m]")
+    >>> plt.ylabel("y[m]")
+    >>> plt.legend()
+    >>> plt.show()
+
+    .. image:: cubic_spline_2d_path.png
+
+    >>> plt.subplots(1)
+    >>> plt.plot(s, [np.rad2deg(iyaw) for iyaw in ryaw], "-r", label="yaw")
+    >>> plt.grid(True)
+    >>> plt.legend()
+    >>> plt.xlabel("line length[m]")
+    >>> plt.ylabel("yaw angle[deg]")
+
+    .. image:: cubic_spline_2d_yaw.png
+
+    >>> plt.subplots(1)
+    >>> plt.plot(s, rk, "-r", label="curvature")
+    >>> plt.grid(True)
+    >>> plt.legend()
+    >>> plt.xlabel("line length[m]")
+    >>> plt.ylabel("curvature [1/m]")
+
+    .. image:: cubic_spline_2d_curvature.png
+    """
+
+    def __init__(self, x, y):
+        self.s = self.__calc_s(x, y)
+        self.sx = CubicSpline1D(self.s, x)
+        self.sy = CubicSpline1D(self.s, y)
+
+    def __calc_s(self, x, y):
+        dx = np.diff(x)
+        dy = np.diff(y)
+        self.ds = np.hypot(dx, dy)
+        s = [0]
+        s.extend(np.cumsum(self.ds))
+        return s
+
+    def calc_position(self, s):
+        """
+        calc position
+
+        Parameters
+        ----------
+        s : float
+            distance from the start point. if `s` is outside the data point's
+            range, return None.
+
+        Returns
+        -------
+        x : float
+            x position for given s.
+        y : float
+            y position for given s.
+        """
+        x = self.sx.calc_position(s)
+        y = self.sy.calc_position(s)
+
+        return x, y
+
+    def calc_curvature(self, s):
+        """
+        calc curvature
+
+        Parameters
+        ----------
+        s : float
+            distance from the start point. if `s` is outside the data point's
+            range, return None.
+
+        Returns
+        -------
+        k : float
+            curvature for given s.
+        """
+        dx = self.sx.calc_first_derivative(s)
+        ddx = self.sx.calc_second_derivative(s)
+        dy = self.sy.calc_first_derivative(s)
+        ddy = self.sy.calc_second_derivative(s)
+        k = (ddy * dx - ddx * dy) / ((dx ** 2 + dy ** 2)**(3 / 2))
+        return k
+
+    def calc_yaw(self, s):
+        """
+        calc yaw
+
+        Parameters
+        ----------
+        s : float
+            distance from the start point. if `s` is outside the data point's
+            range, return None.
+
+        Returns
+        -------
+        yaw : float
+            yaw angle (tangent vector) for given s.
+        """
+        dx = self.sx.calc_first_derivative(s)
+        dy = self.sy.calc_first_derivative(s)
+        yaw = math.atan2(dy, dx)
+        return yaw
+
+
+def calc_spline_course(x, y, ds=0.1):
+    sp = CubicSpline2D(x, y)
+    s = list(np.arange(0, sp.s[-1], ds))
+
+    rx, ry, ryaw, rk = [], [], [], []
+    for i_s in s:
+        ix, iy = sp.calc_position(i_s)
+        rx.append(ix)
+        ry.append(iy)
+        ryaw.append(sp.calc_yaw(i_s))
+        rk.append(sp.calc_curvature(i_s))
+
+    return rx, ry, ryaw, rk, s
+
+def main():
+    print(__file__ + " start!!")
+
+    # start and goal position
+    sx = 5.0  # [m]
+    sy = 0.0  # [m]
+    gx = 90.0  # [m]
+    gy = 50.0  # [m]
+    grid_size = 5.0  # [m]
+    robot_radius = 2.0  # [m]
+
+    # set obstacle positions
+    ox, oy = [], []
+    for i in range(-10, 100):
+        ox.append(i)
+        oy.append(-10.0)
+        
+    for i in range(-10,100):
+        ox.append(i)
+        oy.append(60.0)
+        
+    for i in range(-10, 60):
+        ox.append(100.0)
+        oy.append(i)
+
+    for i in range(-10, 61):
+        ox.append(-10.0)
+        oy.append(i)
+    
+    # obstacles    
+    for i in range(-10, 40):
+        ox.append(20.0)
+        oy.append(i)
+        
+    for i in range(0, 40):
+        ox.append(40.0)
+        oy.append(60.0 - i)
+    
+    for i in range(-10, 40):
+        ox.append(60.0)
+        oy.append(i)
+    
+    for i in range(0, 40):
+        ox.append(80.0)
+        oy.append(60.0 - i)
+
+
+    if show_animation:  # pragma: no cover
+        plt.plot(ox, oy, ".k")
+        plt.plot(sx, sy, "og")
+        plt.plot(gx, gy, "xb")
+        plt.grid(True)
+        plt.axis("equal")
+
+    a_star = AStarPlanner(ox, oy, grid_size, robot_radius)
+    rx, ry = a_star.planning(sx, sy, gx, gy)
+
+    # for curve_fit
+    int_rx = list(map(int,rx))
+    int_ry = list(map(int,ry))
+    # print(rx,ry)
+    print(int_rx,int_ry)
+    rx_array, ry_array = np.array(int_rx), np.array(int_ry)
+    # print(rx_array)
+    popt,pcov = curve_fit(func,rx_array,ry_array)
+    print(popt)
+
+    #interpolation
+    ds = 0.1
+    sp = CubicSpline2D(rx, ry)
+    s = np.arange(0, sp.s[-1], ds)
+
+    bix, biy, biyaw, bik = [], [], [], []
+    for i_s in s:
+        ix, iy = sp.calc_position(i_s)
+        bix.append(ix)
+        biy.append(iy)
+        biyaw.append(sp.calc_yaw(i_s))
+        bik.append(sp.calc_curvature(i_s))
+
+    # plt.subplots(1)
+    # plt.plot(bix, biy, "xb", label="Data points")
+    # plt.plot(bix, biy, "-r", label="Cubic spline path")
+    # plt.grid(True)
+    # plt.axis("equal")
+    # plt.xlabel("x[m]")
+    # plt.ylabel("y[m]")
+    # plt.legend()
+
+
+    if show_animation:  # pragma: no cover
+        plt.plot(bix, biy, "xb")
+        plt.plot(rx_array, func(rx_array,*popt), "k--")
+        plt.plot(rx, ry, "-r")
+        plt.pause(0.001)
+        plt.show()
+
+
+if __name__ == '__main__':
+    main()
